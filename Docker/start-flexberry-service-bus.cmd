fe3d83670ba940a8f06e5578d350b75e27b2c6bf
akosinsky <akosinsky@ICS-KK12324>
2018-03-14 16:45:46 +0500

    Fix start-flexberry-service-bus.cmd

--- Docker/start-flexberry-service-bus.cmd
+++ Docker/start-flexberry-service-bus.cmd
@@ -1,5 +1,14 @@
 docker pull flexberry/flexberry-service-bus-editor:latest
 docker pull flexberry/flexberry-service-bus-postgres-db:latest
 docker pull flexberry/flexberry-service-bus:latest
-docker swarm init
-docker stack  deploy -c flexberry-service-bus-swarm-configuration.yml FlexberryServiceBus
+docker stack ls >NUL  2>NUL
+IF %ERRORLEVEL% NEQ 0 (
+	docker swarm init
+)
+docker stack ls | findstr FlexberryServiceBus
+IF %ERRORLEVEL% EQU 0 (
+	echo Flexberry Service Bus is already started.
+	echo To stop the Flexberry Service Bus, you need to run the command 'stop-flexberry-service-bus.cmd'
+) ELSE (
+	docker stack  deploy -c flexberry-service-bus-swarm-configuration.yml FlexberryServiceBus
+)
