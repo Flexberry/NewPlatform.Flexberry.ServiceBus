cd681121298f8278a4800229568ca58296c71fdf
Eugene Kolegov <ekolegov@outlook.com>
2018-11-16 18:04:38 +0500

    Add flexberry-service-bus-rabbitmq swarm configuration

--- Docker/start-flexberry-service-bus.cmd
+++ Docker/start-flexberry-service-bus.cmd
@@ -1,14 +1,24 @@
 docker pull flexberry/flexberry-service-bus-editor:latest
 docker pull flexberry/flexberry-service-bus-postgres-db:latest
 docker pull flexberry/flexberry-service-bus:latest
+
+IF %1 EQU rabbitmq (
+	docker pull flexberry/rabbitmq:latest
+)
+
 docker stack ls >NUL  2>NUL
 IF %ERRORLEVEL% NEQ 0 (
 	docker swarm init
 )
+
 docker stack ls | findstr FlexberryServiceBus
 IF %ERRORLEVEL% EQU 0 (
 	echo Flexberry Service Bus is already started.
 	echo To stop the Flexberry Service Bus, you need to run the command 'stop-flexberry-service-bus.cmd'
 ) ELSE (
-	docker stack  deploy -c flexberry-service-bus-swarm-configuration.yml FlexberryServiceBus
+	IF %1 EQU rabbitmq (
+		docker stack deploy -c flexberry-service-bus-rabbitmq.yml FlexberryServiceBus
+	) ELSE (
+		docker stack deploy -c flexberry-service-bus-swarm-configuration.yml FlexberryServiceBus
+	)
 )
