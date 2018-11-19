f90072021bcc00db9d70136805659a592565704d
Eugene Kolegov <ekolegov@outlook.com>
2018-11-19 13:27:36 +0500

    Fix docker images files change line endings from CRLF to LF

--- Docker/flexberry-service-bus-postgres-db/host/createdb.sh
+++ Docker/flexberry-service-bus-postgres-db/host/createdb.sh
@@ -7,4 +7,3 @@ until psql -U postgres <create.sql; do echo "Wait...";sleep 2; done
 until psql -U flexberryservicebususer -d flexberryservicebus <CreateNewDB.sql; do echo "Wait...";sleep 5; done 
 
 /etc/init.d/postgresql stop;  while su -c psql postgres </dev/null >/dev/null 2>&1; do sleep 1; done; echo "postgresql stopped"
-
