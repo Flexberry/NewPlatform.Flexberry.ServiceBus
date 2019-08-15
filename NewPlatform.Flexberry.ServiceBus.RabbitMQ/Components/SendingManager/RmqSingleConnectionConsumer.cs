10cabf68e6e407798427fccc42bfe624273dafdc
BatNiy <batyrov.n@gmail.com>
2019-08-15 17:44:32 +0500

    Added consumer's single connection feature

---
+++ NewPlatform.Flexberry.ServiceBus.RabbitMQ/Components/SendingManager/RmqSingleConnectionConsumer.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+
+namespace NewPlatform.Flexberry.ServiceBus.Components
+{
+    internal class RmqSingleConnectionConsumer : BaseRmqConsumer
+    {
+        private readonly IConnection _connection;
+
+        protected override IConnection Connection => _connection;
+
+        public RmqSingleConnectionConsumer(ILogger logger, IMessageConverter converter, IConnection connection,
+            Subscription subscription, ushort defaultPrefetchCount, bool useLegacySenders) : base(logger, converter, subscription, defaultPrefetchCount, useLegacySenders)
+        {
+            _connection = connection;
+        }
+    }
+}
