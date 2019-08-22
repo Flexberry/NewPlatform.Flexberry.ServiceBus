1f6f47c38a2d595de73d0f6bb36ef1c7d4ce554b
BatNiy <batyrov.n@gmail.com>
2019-08-22 19:21:55 +0500

    Added connection factory for manual setting rmq cluster's addresses

---
+++ NewPlatform.Flexberry.ServiceBus.RabbitMQ/Components/RabbitMq.Client/MultihostConnectionFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewPlatform.Flexberry.ServiceBus.Components
+{
+    using RabbitMQ.Client;
+    using System.Linq;
+
+    /// <summary>
+    /// Connection factory for creating connection to one specified URI.
+    /// </summary>
+    public class MultihostConnectionFactory : ConnectionFactory
+    {
+        private AmqpTcpEndpoint[] _endpoints;
+
+        /// <summary>
+        ///  
+        /// </summary>
+        /// <param name="endpoints">AMQP URIs</param>
+        public MultihostConnectionFactory(Uri[] endpoints) : this(endpoints.Select(x => new AmqpTcpEndpoint(x)).ToArray())
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoints">AMQP URIs</param>
+        public MultihostConnectionFactory(AmqpTcpEndpoint[] endpoints)
+        {
+            this._endpoints = endpoints;
+        }
+
+        /// <summary>
+        /// Create connection to one of specified endpoint.
+        /// </summary>
+        /// <returns></returns>
+        public override IConnection CreateConnection()
+        {
+            return base.CreateConnection(_endpoints);
+        }
+    }
+}
